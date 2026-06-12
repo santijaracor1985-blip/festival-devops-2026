7eef8b3391fe2b3edbc10dd18889f19fd46cb3d7
santiago jaramillo <santijaracor1985@gmail.com>
2026-06-12 07:27:27 -0500

    Se mejora API Flask del festival

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,23 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def inicio():
+    return jsonify({
+        "mensaje": "Bienvenido a Pacific DevOps Music Fest API"
+    })
+
 @app.route("/api")
 def concierto():
     return jsonify({
-        "festival":"Pacific DevOps Music Fest",
-        "fecha":"15 Agosto 2026",
-        "artistas":[
+        "festival": "Pacific DevOps Music Fest",
+        "fecha": "15 Agosto 2026",
+        "artistas": [
             "Imagine Dragons",
             "Coldplay",
             "Linkin Park"
         ]
     })
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
